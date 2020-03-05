b6d5b9899756d9bb7a5dde29e48f2c984495c9ee
hansa-lallu <hansalallu@gmail.com>
2020-03-05 12:28:53 +0000

    step 8 - limiting each ntoe to 20 characters on the main note list page

--- js/note-list-view-model.js
+++ js/note-list-view-model.js
@@ -6,7 +6,7 @@ NoteListView.prototype = (function() {
   function displayNoteView() {
     var note = '';
     for(let i = 0; i < this.noteList.displayList().length; i++) {
-      note += '<li><div>' + this.noteList.displayList()[i].displayNote() + '</div></li>'
+      note += '<li><div>' + this.noteList.displayList()[i].displayNote().substring(0,20) + '</div></li>'
     }
     return '<ul>' + note + '</ul>'
   }
--- spec/note-list-view-model-test.js
+++ spec/note-list-view-model-test.js
@@ -1,11 +1,18 @@
 describe("Note list view", () => {
+
   var noteList = new NoteList();
-  noteList.createNote('Favourite food: pesto')
+  noteList.createNote('Favourite food: pesto pizza')
   var noteListView = new NoteListView(noteList);
-  it('instantiates with a note list', () => {
-  expect(noteListView.noteList).toBeInstanceOf(NoteList)
+
+    it('instantiates with a note list', () => {
+    expect(noteListView.noteList).toBeInstanceOf(NoteList)
+    })
+
+    it('returns list of items in html tags', () => {
+    expect(noteListView.displayNoteView()).toBe('<ul><li><div>Favourite food: pest</div></li></ul>')
   })
-  it('returns list of items in html tags', () => {
-    expect(noteListView.displayNoteView()).toBe('<ul><li><div>Favourite food: pesto</div></li></ul>')
-  })
+
+    it('limits characters of note to 20', () => {
+      expect(noteListView.displayNoteView()).toBe('<ul><li><div>Favourite food: pest</div></li></ul>')
+    })
 });
