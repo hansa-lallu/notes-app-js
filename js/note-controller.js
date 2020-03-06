cbcaeccd42fcdb2e5d99c3c128125366c7d9cdf8
Liz Daly <liz7daly@gmail.com>
2020-03-06 14:18:27 +0000

    Lead single note view on click

--- js/note-controller.js
+++ js/note-controller.js
@@ -7,13 +7,38 @@ function NoteController(noteList) {
 NoteController.prototype = (function() {
   function insertHTML() {
     document.getElementById('app').innerHTML = this.noteListView.displayNoteView()
-    
   }
+
+  function loadNote() {
+    window.addEventListener("hashchange", showTextofSelectedNote);
+  }
+
+  function showTextofSelectedNote() {
+    showText(getNoteFromURL(window.location))
+  }
+
+  function getNoteFromURL(location) {
+    var id = parseInt(location.hash.split("#")[1])
+
+    return this.noteList.findNote(id)
+  }
+
+  function showText(note) {
+
+    singleNoteView = new SingleNoteView(note)
+
+    document.getElementById('note').innerHTML = singleNoteView.displaySingleNote()
+  }
+
   return {
-    insertHTML: insertHTML
+    insertHTML: insertHTML,
+    loadNote: loadNote,
+    showText: showText
   }
 })();
 
 noteList = new NoteList();
 noteController = new NoteController(noteList);
+noteController.insertHTML();
+noteController.loadNote();
 
--- spec/note-controller-test.js
+++ spec/note-controller-test.js
@@ -8,15 +8,20 @@ describe ("note controller", function() {
 
   it("adds a note to the note list", function() {
     thisNote = noteController.noteList.storedNotes[0]
-    noteController.insertHTML();
     expect(document.getElementById('app').innerHTML).toBe(`<ul><li><a href="#${thisNote.id}">Favourite drink: sel</a></li></ul>`)
   })
 
   it("adds more than one note to the view", function() {
-    note1 = noteController.noteList.storedNotes[0]
     noteController.noteList.createNote("Buy some milk Ziad!")
+    note1 = noteController.noteList.storedNotes[0]
     note2 = noteController.noteList.storedNotes[1]
     noteController.insertHTML();
     expect(document.getElementById('app').innerHTML).toBe(`<ul><li><a href="#${note1.id}">Favourite drink: sel</a></li><li><a href="#${note2.id}">Buy some milk Ziad!</a></li></ul>`)
+  })
+  
+  it("loads a note when clicking on the link for it", function() {
+    note1 = noteController.noteList.storedNotes[0]
+    noteController.loadNote(note1);
+    expect(document.getElementById('note').innerHTML).toBe("<div>Favourite drink: seltzer</div>")
   })
 });
