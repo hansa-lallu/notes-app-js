fe3d2a2d79d213a47225c645ae04a358ee947dac
hansa-lallu <hansalallu@gmail.com>
2020-03-06 10:34:16 +0000

    added unique id + colours for tests

--- js/note-model.js
+++ js/note-model.js
@@ -1,13 +1,17 @@
-function Note(text) {
-  this.text = text
-};
+(function (exports){
+  
+  var noteIdCounter = 1 
+  
+  function Note(text){
+    this.text = text
+    this.id = noteIdCounter;
+    noteIdCounter += 1
+  }
 
-Note.prototype = (function() {
-  function displayNote() {
-    return this.text
-  };
+  exports.Note = Note;
 
-  return {
-    displayNote: displayNote
+  Note.prototype.displayNote = function() {
+    return this.text;
   };
-})();
+  
+})(this);
--- spec/note-model-test.js
+++ spec/note-model-test.js
@@ -1,8 +1,22 @@
+
 describe("Note", () => {
 
-  var note = new Note("My favourite language is javascript!");
-
   it("takes a note", () => {
+    var note = new Note("My favourite language is javascript!");
     expect(note.displayNote()).toBe("My favourite language is javascript!");
   });
+
+
+describe("Gives a note an id", () => {
+  it("is instantiated with an id", () => {
+    var note = new Note("My favourite language is javascript");
+    expect(note.id).NotToBe(null)
+    })
+  });
+
+  it("note id is unique", () => {
+    var note1 = new Note("My favourite language is javascript");
+    var note2 = new Note("My favourite language is Spanish");
+    expect(note1.id).NotToBe(note2.id)
+  })
 });
